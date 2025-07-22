76480f99cf183df603d8ac374eeb7b5f1f040d6b
Joodi <winbeta.com@gmail.com>
2025-07-22 21:58:31 +0330

    Update index.ts

--- front/src/reducer/post/index.ts
+++ front/src/reducer/post/index.ts
@@ -6,50 +6,51 @@ export const postReducer = (state: PostState, action: PostAction) => {
         case PostActionType.ADD_POST: {
             const { id, title, content } = action.payload as Post;
             return {
-                    selectedPost: InitialPostState.selectedPost,
-                    posts: [
-                        ...state.posts,
-                        {
-                            id, title, content
-                        }
-                    ]
+                selectedPost: InitialPostState.selectedPost,
+                posts: [
+                    ...state.posts,
+                    {
+                        id, title, content
+                    }
+                ]
             }
         }
         case PostActionType.EDIT_POST: {
             const { id, title, content } = action.payload as Post;
             return {
-                    selectedPost: InitialPostState.selectedPost,
-                    posts: state.posts.map((post)=> {
-                        if (post.id === id){
-                            return {
-                                id,
-                                title,
-                                content
-                            }
+                selectedPost: InitialPostState.selectedPost,
+                posts: state.posts.map((post) => {
+                    if (post.id === id) {
+                        return {
+                            id,
+                            title,
+                            content
                         }
-                        return post
-                    })
+                    }
+                    return post
+                })
             }
         }
         case PostActionType.DELETE_POST: {
             const id = action.payload;
             return {
-                    selectedPost: InitialPostState.selectedPost,
-                    posts: [...state.posts.filter((post)=> post.id !== id?.toString())]
+                selectedPost: InitialPostState.selectedPost,
+                posts: [...state.posts.filter((post) => post.id !== id?.toString())]
             }
         }
         case PostActionType.SET_POST: {
-            return {...state, selectedPost: action.payload}
+            return { ...state, selectedPost: action.payload }
         }
         case PostActionType.SET_POSTS: {
-            return {selectedPost: InitialPostState.selectedPost, posts: action.payload}
+            return { selectedPost: InitialPostState.selectedPost, posts: action.payload }
         }
         case PostActionType.GET_POSTS: {
             return { ...state };
-          }
+        }
         default: {
-            const otherAction = action.type;
-            return otherAction;
+            // const otherAction = action.type;
+            // return otherAction;
+            return state;
         }
     }
 }
